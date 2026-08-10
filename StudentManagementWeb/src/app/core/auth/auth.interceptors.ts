2ffb8082b795955ad2ed74c55eb72ec48ff4976b
Jorge Simon Quintero Munoz <jorgesquinterom@interrapidisimo.com>
2026-08-10 13:07:15 -0500

    Reobtener csrf

--- StudentManagementWeb/src/app/core/auth/auth.interceptors.ts
+++ StudentManagementWeb/src/app/core/auth/auth.interceptors.ts
@@ -2,10 +2,12 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, switchMap, throwError } from 'rxjs';
+import { ApiProblemDetails } from '../api/api.models';
 import { AntiforgeryService } from './antiforgery.service';
 import { SessionState } from './session-state.service';
 
 const SAFE_METHODS = new Set(['GET', 'HEAD', 'OPTIONS']);
+const ANTIFORGERY_ERROR_CODE = 'InvalidAntiforgeryToken';
 
 export const credentialsAndAntiforgeryInterceptor: HttpInterceptorFn = (request, next) => {
   const credentialRequest = request.clone({ withCredentials: true });
@@ -13,18 +15,35 @@ export const credentialsAndAntiforgeryInterceptor: HttpInterceptorFn = (request,
     return next(credentialRequest);
   }
 
-  return inject(AntiforgeryService)
-    .getToken()
-    .pipe(
-      switchMap((token) =>
-        next(
-          credentialRequest.clone({
-            setHeaders: { 'X-CSRF-TOKEN': token },
-          }),
+  const antiforgery = inject(AntiforgeryService);
+  const sendWithToken = () =>
+    antiforgery
+      .getToken()
+      .pipe(
+        switchMap((token) =>
+          next(credentialRequest.clone({ setHeaders: { 'X-CSRF-TOKEN': token } })),
         ),
-      ),
-    );
+      );
+
+  return sendWithToken().pipe(
+    catchError((error: unknown) => {
+      if (!isAntiforgeryError(error)) {
+        return throwError(() => error);
+      }
+     
+      antiforgery.clear();
+      return sendWithToken();
+    }),
+  );
 };
+
+function isAntiforgeryError(error: unknown): boolean {
+  return (
+    error instanceof HttpErrorResponse &&
+    error.status === 400 &&
+    (error.error as ApiProblemDetails | null)?.code === ANTIFORGERY_ERROR_CODE
+  );
+}
 
 export const sessionInterceptor: HttpInterceptorFn = (request, next) => {
   const session = inject(SessionState);
